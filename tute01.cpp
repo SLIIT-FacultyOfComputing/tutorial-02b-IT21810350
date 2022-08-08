ac1e896b6bec58ed5dff26b19b104eaf3996480a
IT21810350 Malshan <92059622+IT21810350@users.noreply.github.com>
2022-08-08 04:09:21 +0000

    completed all the exercises in tutorial 2

--- tute01.cpp
+++ tute01.cpp
@@ -18,13 +18,20 @@ printf("Hello World") --> std::cout << "Hello World";
 */
 
 
-#include <stdio.h>
-int main() 
-{
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+
     float cm, inches;
-    printf("Enter a length in cm : ");
-    scanf("%f",&cm);
-    inches = cm / 2.54;
-    printf("Length in inches is %f \n", inches);
+  
+    cout << "Enter a length in cm: ";
+    cin >> cm;
+  
+    inches = cm /2.54;
+  
+    cout << "Length in inches is " << setprecision(2) << inches << endl;
+  
     return 0;
-}  
+}
